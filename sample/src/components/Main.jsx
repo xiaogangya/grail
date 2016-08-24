0d1d4265d9923cacc4647b62b6e1fb59e5815a60
yanz <yanz@microsoft.com>
2016-08-25 01:57:40 +0800

    Rename to jsx.

---
+++ sample/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {count: 0};
+    this.tick = this.tick.bind(this);
+  }
+  tick() {
+    this.setState({count: this.state.count + 1});
+  }
+  render() {
+    return (
+      <button onClick={this.tick}>
+        Clicks: {this.state.count}
+      </button>
+    );
+  }
+}
